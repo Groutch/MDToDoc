18d9313d7b85b8ecc7620ed0a2a3cca948c8a7b7
clement-scherrer <clement.scherrer@new-immo-group.com>
2025-09-13 13:45:03 +0200

    fix: quote

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function HomePage() {
       <section className="lg:col-span-3">
         <h1 className="text-2xl font-semibold mb-4">Bienvenue 👋</h1>
         <p className="text-neutral-300 mb-4">
-          Voici la doc <span className="font-semibold">OFFICIELLE</span> de l'équipe Alpha. Tu peux commencer par{" "}
+          Voici la doc <span className="font-semibold">OFFICIELLE</span> de l&apos;équipe Alpha. Tu peux commencer par{" "}
           <Link className="underline" href="/index">index.md</Link>.
         </p>
       </section>
